f45c0aedacfffcb46a2a8334019ae94e5de5e1b0
노현 박 <pa0101@live.co.kr>
2023-03-20 12:56:41 +0900

    gcd

---
+++ gcd/gcd/gcd.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int euclid(int a, int b) //최대공약수 구하는 함수 선언
+{
+    if (b == 0) return a;
+    return euclid(b, a % b);
+}
+
+int test1_euclid()  //print 함수 호출하는 함수 선언
+{
+    int gcd = 0;
+    gcd = euclid(34, 48); //euclid 함수안에 변수 선언 및 gcd에 저장
+    printf("%d", gcd);
+    return 0;
+}
+
+int main(int argc, char* argv[]) //메인 함수
+{
+    test1_euclid(); //printf 함수 호출
+    return 0;
+}
