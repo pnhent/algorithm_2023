f99277bc3b07119007558bfdce76be5ff2e75630
노현 박 <pa0101@live.co.kr>
2023-04-18 14:16:48 +0900

    circlepad hanoi_Tower

---
+++ hanoitop_program/hanoitop_program/hanoitop_program.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+
+void hanoi_top(int n, char from, char tmp, char to);
+
+int main(void)
+{
+	hanoi_top(4, 'A', 'B', 'C');
+}
+
+void hanoi_top(int n, char from, char tmp, char to)
+{
+	if (n == 1)
+		printf("1 circlepad %c to %c set.\n", from, to);
+	else
+	{
+		hanoi_top(n - 1, from, to, tmp);
+		printf("circlepad %d %c to %c set.\n", n, from, to);
+		hanoi_top(n - 1, tmp, from, to);
+	}
+}
