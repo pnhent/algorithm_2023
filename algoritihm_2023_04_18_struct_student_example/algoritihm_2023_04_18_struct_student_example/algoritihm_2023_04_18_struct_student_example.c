4a12ab7c547f6957be4b4922bc9183964c2ea3a9
노현 박 <pa0101@live.co.kr>
2023-04-18 14:57:47 +0900

    struct Point

---
+++ algoritihm_2023_04_18_struct_student_example/algoritihm_2023_04_18_struct_student_example/algoritihm_2023_04_18_struct_student_example.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _Point
+{
+	int x;
+	int y;
+	int z;
+} Point;
+
+Point myPoints[10] = { {0,0,0} };
+Point yourPoints[] = { {1,2,3},{4,5,6} };
+
+Point sum(Point argPoints[], int size)
+{
+	Point result = { 0,0,0 };
+
+	for (int i = 0; i < size; i++)
+	{
+		result.x = result.x + argPoints[i].x;
+		result.y += argPoints[i].y;
+		result.z += argPoints[i].z;
+		return result;
+	}
+}
+
+int main(void)
+{
+	int size = sizeof(yourPoints) / sizeof(yourPoints[0]);
+	sum(yourPoints, size);
+	return 0;
+}
+
