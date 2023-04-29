21f7fc09bd2c284134d7dfce651d7d984e5c16a4
노현 박 <pa0101@live.co.kr>
2023-04-29 21:09:20 +0900

    error

---
+++ C express folder/example 2.7/error4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("마트에서 사올 품목\n");
+    printf("===============\n");
+    printf("사과, 우유, 빵\n");
+    printf("===============\n");
+
+    return 0;
+}
