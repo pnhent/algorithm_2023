e56eb6fe4011e430940431ec2b4e308b8159666b
노현 박 <pa0101@live.co.kr>
2023-04-29 22:05:21 +0900

    programming 05

---
+++ C express folder/Chapter 2/chapter2 Programming 05/programming05.c
@@ -0,0 +1,13 @@
+//문제 : 7과 9을 가지고 사칙 연산을 한 결과를 다음과 같이 출력하는 프로그램을 작성해보시오.
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("7+8=%d\n",7+8);
+    printf("7-8=%d\n",7-8);
+    printf("7*8=%d\n",7*8);
+    printf("7/8=%d\n",7/8);
+
+    return 0;
+}
