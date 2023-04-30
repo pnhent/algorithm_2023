60880c89f681a57de3d9bb89c8f3f1e31c3c7e77
노현 박 <pa0101@live.co.kr>
2023-04-30 20:24:41 +0900

    programming05

--- C express folder/Chapter 2/chapter2 Programming 05/programming05.c
+++ C express folder/Chapter 2/chapter2 Programming 05/programming05.c
@@ -4,10 +4,10 @@
 
 int main(void)
 {
-    printf("7+8=%d\n",7+8);
-    printf("7-8=%d\n",7-8);
-    printf("7*8=%d\n",7*8);
-    printf("7/8=%d\n",7/8);
+    printf("7+9=%d\n",7+9);
+    printf("7-9=%d\n",7-9);
+    printf("7*9=%d\n",7*9);
+    printf("7/9=%d\n",7/9);
 
     return 0;
 }
