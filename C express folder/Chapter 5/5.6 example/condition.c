31b3f29fd3be3fa9eb2ce7e1f845271d487e4734
노현 박 <pa0101@live.co.kr>
2023-05-01 04:54:44 +0900

    5.6 example

---
+++ C express folder/Chapter 5/5.6 example/condition.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x,y;
+
+    printf("정수 2개: ");
+    scanf("%d %d", &x, &y);
+
+    printf("큰 수=%d \n", (x > y) ? x : y);
+    printf("작은 수=%d \n", (x < y) ? x : y);
+    
+    return 0;
+}
