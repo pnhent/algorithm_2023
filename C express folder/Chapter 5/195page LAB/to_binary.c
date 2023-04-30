f80aea65d546ba69a7c9948a09ef5e4909e6e73e
노현 박 <pa0101@live.co.kr>
2023-05-01 05:22:53 +0900

    195page LAB

---
+++ C express folder/Chapter 5/195page LAB/to_binary.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned int num;
+    printf("십진수: ");
+    scanf("%u", &num);
+
+    unsigned int mask = 1 << 7;
+    printf("이진수: ");
+
+    ((num & mask) == 0) ? printf("0") : printf("1");
+    mask = mask >> 1;
+    ((num & mask) == 0) ? printf("0") : printf("1");
+    mask = mask >> 1;
+    ((num & mask) == 0) ? printf("0") : printf("1");
+    mask = mask >> 1;
+    ((num & mask) == 0) ? printf("0") : printf("1");
+    mask = mask >> 1;
+    ((num & mask) == 0) ? printf("0") : printf("1");
+    mask = mask >> 1;
+    ((num & mask) == 0) ? printf("0") : printf("1");
+    mask = mask >> 1;
+    ((num & mask) == 0) ? printf("0") : printf("1");
+    mask = mask >> 1;
+    printf("\n");
+
+    return 0;
+}
