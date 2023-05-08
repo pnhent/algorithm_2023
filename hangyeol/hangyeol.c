5fbcbceab5d66587de0b2433f4039346e67f33c7
노현 박 <pa0101@live.co.kr>
2023-05-08 12:24:09 +0900

    hangyeol

---
+++ hangyeol/hangyeol.c
@@ -0,0 +1,11 @@
+#define INFN (9292929390)
+const char * Nodename[4] = {"n0", "n1", "n2", "n3"};
+
+int G[4][4] = {
+    {0, 3, INFN, INFN},
+    {INFN, 0, 4, INFN},
+    {INFN, INFN, 0, 5},
+    {INFN, INFN, 2, 0}
+};
+
+
