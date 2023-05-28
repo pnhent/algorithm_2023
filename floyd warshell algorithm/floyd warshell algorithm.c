0a2088f24f7ffb123629908337a34c4f1b82c70c
노현 박 <pa0101@live.co.kr>
2023-05-28 21:42:56 +0900

    floydwarshell algorithm

--- floyd warshell algorithm/floyd warshell algorithm.c
+++ floyd warshell algorithm/floyd warshell algorithm.c
@@ -1,39 +1,50 @@
 #include <stdio.h>
+#define INF 99999 
+#define V 5 
 
-#define V 5
-#define infinity 100000
+void printSolution(int dist[][V]);
 
-int min(int lhs, int rhs){
-    return  lhs < rhs ? lhs : rhs;
-}
-
-int floyd_warshell(int dist[][V])
-{
-
-    for(int k = 1; k<= V; k++)
-    {
-        for(int i = 1; i <= V; i++)
-        {
-            for(int j = 1; j<=V; j++)
-            {
-                dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j]);
+void floydWarshall (int graph[][V]) {
+    int dist[V][V], i, j, k;
+ 
+    for (i = 0; i < V; i++)
+        for (j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+ 
+    for (k = 0; k < V; k++) {
+        for (i = 0; i < V; i++) {
+            for (j = 0; j < V; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
             }
         }
     }
-} 
+ 
+    printSolution(dist);
+}
 
-int main (void)
-{
-    int graph[5][5] =
-    {
-        {0, 3, infinity, 2, 1},
-        {infinity, 0, 1, infinity, 1},
-        {infinity, 1, 0, 6, 5},
-        {3, infinity, 6, 0, infinity},
-        {infinity, 1, 5, 1, 0}
-    };
+void printSolution(int dist[][V]) {
+    printf ("다음 행렬은 각 정점 쌍 간의 최단 거리를 보여줍니다. \n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("%7s", "INF");
+            else
+                printf ("%7d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
 
-    
-
-    floyd_warshell(graph);   
+int main() {
+    int graph[V][V] = 
+    {   {0, 3, INF, 2, 1},
+        {INF, 0, 1, INF, 1},
+        {INF, 1, 0, 6, 5},
+        {3, INF, 6, 0, INF},
+        {INF, 1, 5, 1, 0}
+        };
+ 
+    floydWarshall(graph);
+    return 0;
 }
